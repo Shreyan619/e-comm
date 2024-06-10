34e51f019ff982b7fdc247576f4761fc86f723ce
Shreyan619 <shreyankundu73@gmail.com>
2024-06-10 10:55:17 +0530

    3rd updated commit

--- frontend/src/components/Product/ProductDetails.jsx
+++ frontend/src/components/Product/ProductDetails.jsx
@@ -1,10 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
+import Carousel from 'react-material-ui-carousel'
+import "./ProdDetails.css"
+import { useDispatch, useSelector } from "react-redux"
+import { getAllProductsDetails } from '../../actions/productAction'
 
-const ProductDetails = () => {
+const ProductDetails = (match) => {
+
+  const dispatch = useDispatch()
+
+  const {}=useSelector(state=>state.productDetails)
+
+  useEffect(() => {
+    dispatch(getAllProductsDetails(match.params.id))
+
+  }, [dispatch,match.params.id])
+
+
   return (
     <Fragment>
-        <div className='ProductDetails'>
-      
+      <div className='ProductDetails'>
+        <Carousel>
+          {product.images && product.images.map((item, i) => (
+            <img className='CarouselImage'
+              key={item.url}
+              alt={`${i} Slide`}
+              src={item.url}
+            />
+          ))}
+        </Carousel>
       </div>
     </Fragment>
   )
